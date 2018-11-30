87a52000198ae325e5e91f4e40df7f82e2d4ca57
Mr-CAD <44128918+Mr-CAD@users.noreply.github.com>
2018-11-30 16:37:36 -0500

    Done!

---
+++ Scene switchable/Switchable scene/Assets/Lobby/SceneSwitchGame1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchGame1 : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+}
+
